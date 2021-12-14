651ca2e2a1363f0b3cbad8ecae76697d35d9ae11
reika727 <reikasuya727@gmail.com>
2021-12-14 19:09:29 +0900

    create test directory

--- obfuscate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-LL=${1%.*}.ll
-OBFUSCATED=${1%.*}.obfuscated.ll
-clang -S -emit-llvm -Xclang -disable-O0-optnone -o $LL $1
-opt -S -load-pass-plugin ./lambdaize-loop.so -passes=lambdaize-loop -o $OBFUSCATED $LL
---
+++ test/obfuscate.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+SCRIPTDIR=$(dirname $(readlink -f $0))
+LL=${1%.*}.ll
+OBFUSCATED=${1%.*}.obfuscated.ll
+make -C $SCRIPTDIR/..
+clang -S -emit-llvm -Xclang -disable-O0-optnone -o $LL $1
+opt -S -load-pass-plugin $SCRIPTDIR/../lambdaize-loop.so -passes=lambdaize-loop -o $OBFUSCATED $LL
