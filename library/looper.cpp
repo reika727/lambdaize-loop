466519eebef0d8246fcba4dd305831c9e54d9a7a
reika727 <reikasuya727@gmail.com>
2021-12-19 02:54:50 +0900

    implement looper_internal_lambda_recursion

--- library/looper.cpp
+++ library/looper.cpp
@@ -1,4 +1,5 @@
 #include <cstdarg>
+#include <utility>
 namespace {
     void looper_internal_recursion(bool (*loopee)(va_list), va_list vl)
     {
@@ -10,12 +11,35 @@ namespace {
         va_end(stored);
         return;
     }
+    void looper_internal_lambda_recursion(bool (*loopee)(va_list), va_list vl)
+    {
+        auto Z = [](auto f) {
+            auto curry = [](auto f) {
+                return [=](auto &&...xs) {
+                    return [&](auto &&...ys) {
+                        return f(std::forward<decltype(xs)>(xs)..., std::forward<decltype(ys)>(ys)...);
+                    };
+                };
+            };
+            return curry(f)(f);
+        };
+        Z([](auto f, bool (*loopee)(va_list), va_list vl) -> void {
+            va_list stored;
+            va_copy(stored, vl);
+            if (loopee(vl)) {
+                f(f, loopee, stored);
+            }
+            va_end(stored);
+            return;
+        })
+        (loopee, vl);
+    }
 }
 extern "C" void looper(bool (*loopee)(va_list), ...)
 {
     va_list vl;
     va_start(vl, loopee);
-    looper_internal_recursion(loopee, vl);
+    looper_internal_lambda_recursion(loopee, vl);
     va_end(vl);
     return;
 }
